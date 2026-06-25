888f3080d5a04f719447839c677c53778f0f2092
TaclarGabon <judicael.edanngomo@gmail.com>
2026-06-25 08:55:31 -0700

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-window.TACLAR_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyC_JNVVGrd2whXRGfHuOlShhmZfaLQFLfw",
-  authDomain: "taclar-52e72.firebaseapp.com",
-  projectId: "taclar-52e72",
-  storageBucket: "taclar-52e72.firebasestorage.app",
-  messagingSenderId: "501812718028",
-  appId: "1:501812718028:web:45842bcde3db9049ec9569"
-};
